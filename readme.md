8067fb6a623d6e9def60dc4b63feb313bb296f20
ling <abc1514671906@163.com>
2024-08-19 17:19:09 +0800

    Init project

--- readme.md
+++ readme.md
@@ -3,3 +3,13 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-gatheringhub-helper?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-gatheringhub-helper)
 
 
+## 仍在开发中..
+
+## 介绍  
+此插件旨在帮助怪猎群友快速发布获取群集会码,免去频繁发公告修改公告的麻烦
+
+## 其他 
+如果有问题欢迎提出issue  
+不管有没有问题都欢迎通过abc1514671906@163.com联系我
+## License
+[MIT](LICENSE) License Copyright (c) 2024 ling
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,58 @@
-import { Context, Schema } from 'koishi'
+import { Session } from 'inspector'
+import { Argv, Context, Schema } from 'koishi'
 
 export const name = 'gatheringhub-helper'
+export const usage =`此插件旨在帮助怪猎群友快速发布获取群集会码,免去频繁发公告修改公告的麻烦`
 
-export interface Config {}
+export const inject = {
+  required: ['database']
+}
+
+export interface Config {
+
+}
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+    ctx.command('jhm','怪物猎人集会码助手')
+    .option('add', '-a <massage:text>  添加新的集会码')
+    .option('remove', '-r <massage:number> 删除指定编号的集会码')
+    .action((argv,massage) =>jhmService(argv,massage))
+    .example('jhm -a 114514191981 将 114514191981 添加到集会码列表中')
+    .example('jhm -r 1 将编号为1的集会码删除')
+}
+
+function jhmService(argv:any, massage: string){
+    console.log(argv.session.event.member+""+massage);
+    if(!massage)
+    {
+        return "未检测到有效输入"
+    }
+    if(argv.options.add)
+    {
+        return
+    }
+    else if(argv.options.remove){
+        return
+    }
+    
+    return jhmShowAll();
+}
+
+/**
+ * 
+ * @returns 返回本群所有集会码信息
+ */
+function jhmShowAll() :string {
+// TODO:根据群号查询集会码信息 
+    return ;
+}
+
+function jhmAdd(){
+    // TODO:给本群添加一个集会码
+}
+
+function jhmRemove(){
+    // TODO:删除本群的指定集会码
 }
