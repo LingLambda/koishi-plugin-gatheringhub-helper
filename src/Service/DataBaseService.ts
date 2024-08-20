2ada09644fe5d5553c512d88e2151b515c664679
ling <abc1514671906@163.com>
2024-08-20 14:17:46 +0800

    完成基本功能

---
+++ src/Service/DataBaseService.ts
@@ -0,0 +1,120 @@
+import { Context } from 'koishi'
+import { $ } from 'koishi'
+
+
+
+  //习惯原因把增删改都加了事务不知道会有什么后果
+ export class DataBaseService{
+
+  /**
+   * 查询所有集会码信息
+   * @param ctx 
+   * @param groupId 群号
+   * @returns 数据表数组
+   */
+   async showInfoByGroupId(ctx: Context, groupId: string) {
+     try {
+       // 查询排序返回
+       return await ctx.database
+         .select('gatheringhub')
+         .where(row => $.eq(row.group_id, groupId))
+         .orderBy(row => row.hub_no)
+         .execute();
+     } catch (error) {
+       console.error('Error fetching data:', error);
+       throw new Error('Unable to fetch data');
+     }
+   }
+  /**
+   * 添加集会码
+   * @param ctx 
+   * @param hubName 集会码
+   * @param userId 添加者id
+   * @param groupId 群号
+   * @param note 备注
+   * @param hubNo 排序编号
+   */
+   async addInfo(ctx: Context, hubName: string, userId: string,groupId:string, note: string, hubNo: number) {
+    await ctx.database.transact(async()=>{
+    //判断是否有指定编号数据,有的话全部后移1个编号
+     const hasData = await ctx.database.get('gatheringhub',
+       {
+         hub_no: [hubNo],
+         group_id: [groupId]
+       }) !== null;
+     if (hasData) {
+       await this.incrementRowNo(ctx,hubNo,groupId,1)
+     }
+     //添加一行数据,返回值是添加的行的完整数据 (包括自动填充的 id 和默认属性等)
+     await ctx.database.create('gatheringhub',
+      {
+        hub_name: hubName,
+        hub_no: hubNo,
+        add_date: new Date(),
+        user_id: userId,
+        group_id: groupId,
+        note: note
+      })
+    })
+    }
+
+    /**
+     * 删除指定编号的集会码,并把后面的所有编号前移1
+     * @param ctx 
+     * @param hubNo 编号
+     * @param groupId 群号
+     * @returns 提示字符串
+     */
+   async removeInfoByNo(ctx: Context, hubNo: number, groupId: string) {
+     //删除指定No.的集会码,并把后面的No.前移
+     const result=await ctx.database.remove('gatheringhub', {
+       hub_no: hubNo,
+       group_id: groupId
+     })
+     await this.incrementRowNo(ctx, hubNo, groupId, -1);
+     return result.matched==0?  "删除失败,没有这个编号喵":"删除成功喵";
+   }
+
+  /**
+   * 将大于指定编号的行的编号全部前移或后移
+   * @param ctx 
+   * @param hubNo 指定的编号
+   * @param groupId 群号
+   * @param num 指定移动的数
+   */
+  async incrementRowNo(ctx:Context,hubNo:number,groupId:string,num:number){
+    await ctx.database.set('gatheringhub',
+      {
+        hub_no:{$gte: hubNo},
+        group_id:{$eq : groupId}
+      },
+      (row)=>(
+      {
+        hub_no:$.add(row.hub_no,num)
+      }))
+  }
+  /**
+   * 初始化数据表
+   * @param ctx 
+   */
+  async dbInit(ctx:Context){
+    //创建表
+    ctx.model.extend('gatheringhub',
+      {
+        hub_id: 'unsigned',
+        hub_name: 'string',
+        hub_no: 'unsigned',
+        add_date: 'timestamp',
+        group_id: 'string',
+        user_id: 'string',
+        note: 'string'
+      },
+      {
+        primary:'hub_id',//设置主键
+        autoInc:true,//主键自增
+        //unique: ['hub_no'],//不可重复约束
+      }
+    )
+  }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { Session } from 'inspector'
-import { Argv, Context, Schema } from 'koishi'
+import {  Context, Schema } from 'koishi'
+import { DataBaseService } from './Service/DataBaseService'
 
 export const name = 'gatheringhub-helper'
-export const usage =`此插件旨在帮助怪猎群友快速发布获取群集会码,免去频繁发公告修改公告的麻烦`
-
+export const usage =`
+此插件旨在帮助怪猎群友快速发布获取群集会码,免去频繁发公告修改公告的麻烦
+TODO:
+数据库存储
+分群存储
+管理员群主可控(可配置)
+指定qq可控(可配置)
+检测输入格式
+编号自动对齐自动插入
+`
 export const inject = {
   required: ['database']
 }
@@ -14,45 +22,95 @@ export interface Config {
 
 export const Config: Schema<Config> = Schema.object({})
 
+declare module 'koishi' {
+    interface Tables {
+        gatheringhub: Gatheringhub
+    }
+}
+
+// 这里是新增表的接口类型
+export interface Gatheringhub {
+    hub_id: number//自增主键
+    hub_name: string//集会码
+    hub_no:number//排序编号
+    add_date: Date//添加时间
+    group_id: string//群号
+    user_id: string//添加人id
+    note: string//备注
+}
+//定义操作数据库对象
+const dbs=new DataBaseService();
 export function apply(ctx: Context) {
-    ctx.command('jhm','怪物猎人集会码助手')
-    .option('add', '-a <massage:text>  添加新的集会码')
-    .option('remove', '-r <massage:number> 删除指定编号的集会码')
-    .action((argv,massage) =>jhmService(argv,massage))
-    .example('jhm -a 114514191981 将 114514191981 添加到集会码列表中')
+    dbs.dbInit(ctx);
+    ctx.command('jhm <massage:string> <note:text>','怪物猎人集会码助手')
+    .option('add', '-a 添加新的集会码')
+    .option('remove', '-r 删除指定编号的集会码')
+    .action((argv,massage,note) =>jhmService(argv,ctx,massage,note))
+    .example('jhm -a 114514191981 冥赤龙 将 114514191981 添加到集会码列表中设置备注为冥赤龙')
     .example('jhm -r 1 将编号为1的集会码删除')
 }
 
-function jhmService(argv:any, massage: string){
-    console.log(argv.session.event.member+""+massage);
-    if(!massage)
-    {
-        return "未检测到有效输入"
+async function jhmService(argv:any,ctx:Context, massage: string,note:string){
+    //console.log(argv.options.add+" "+argv.options.remove+" "+massage);
+    //console.log(argv.session.onebot);
+    if(!argv.session.onebot){
+        return "暂仅支持onebot适配器喵"
     }
-    if(argv.options.add)
-    {
-        return
+
+    //群号
+    const groupId=argv.session.onebot.group_id;
+    //发送者id
+    const userId=argv.session.onebot.user_id;
+    //发送者身份 owner 或 admin 或 member
+    const userRole= argv.session.onebot.sender.role
+    //是否为群聊
+    const isGroup=argv.session.onebot.message_type=='group'?true:false;
+
+    const hubNo=1
+
+    if(!isGroup){
+        return '非群聊环境无法使用集会码助手喵';
+    }
+    if(argv.options.add){
+        if(!massage||massage.length!=12){
+            return '输入的集会码长度错误喵';
+        }
+        return await jhmAdd(ctx,massage,note,userId,groupId,hubNo);
     }
     else if(argv.options.remove){
-        return
+        return await jhmRemove(ctx,massage,groupId);
     }
-    
-    return jhmShowAll();
+    return await jhmShowAll(ctx,groupId);
 }
 
 /**
  * 
  * @returns 返回本群所有集会码信息
  */
-function jhmShowAll() :string {
-// TODO:根据群号查询集会码信息 
-    return ;
+async function jhmShowAll(ctx:Context, groupId:string){
+        const gatheringhubArray=await dbs.showInfoByGroupId(ctx,groupId)
+        let gatheringhubList:string="编号     集会码          备注\n"
+        gatheringhubArray.forEach(element => {
+            gatheringhubList+=(
+                element.hub_no+"  "+
+                element.hub_name+"  "+
+                element.note+"\n"          
+            )
+        });
+        return gatheringhubList
+
 }
 
-function jhmAdd(){
+async function jhmAdd(ctx:Context,massage:string,note:string,userId:string,groupId:string,hubNo:number){
+    if(!note){
+        note="无"
+    }
     // TODO:给本群添加一个集会码
+   dbs.addInfo(ctx, massage, userId,groupId, note,hubNo);
+   return "添加成功喵"
 }
 
-function jhmRemove(){
-    // TODO:删除本群的指定集会码
+async function jhmRemove(ctx: Context, massage: string, groupId: string) {
+    const hubNo = parseInt(massage, 10)
+    return dbs.removeInfoByNo(ctx, hubNo, groupId);
 }
